95967e31425e0b8a98483be923c94821ff120f41
Henrik Eneroth <henrik@eneroth.com>
2014-01-05 23:40:37 +0100

    Trying uberjar compilation for Heroku

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ns-tracker "0.2.1"]
