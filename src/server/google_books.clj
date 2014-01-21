0a7060100dfc6741754077f58804ece4de3d308b
Henrik Eneroth <henrik@eneroth.com>
2014-01-21 16:24:27 +0100

    URL-encoding search string for Google

--- src/server/google_books.clj
+++ src/server/google_books.clj
@@ -2,20 +2,22 @@
   (:require [org.httpkit.client :as http]
             [clojure.pprint :refer [pprint]]
             [clojure.data.json :as json]
+            [ring.util.codec :refer [url-encode]]
             [server.helper :refer [kebabize-keys]]
             [clojure.walk :refer [keywordize-keys]]))
 
 (def api-key "AIzaSyDzJOr0Xk-fM8gCoLu7DwxTwvIv6V5dG-o")
 
 (def test-url "https://www.googleapis.com/books/v1/volumes?q=neuromancer&download=epub&
-                filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&
-                printType=books&key=")
+              filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&
+              printType=books&key=")
 
 (defn search-url
   [search-string]
   (let [base-url "https://www.googleapis.com/books/v1/volumes"
-        rest-url "&download=epub&filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&printType=books&key="]
-    (str base-url "?q=" search-string rest-url api-key)))
+        rest-url "&download=epub&filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&printType=books&key="
+        encoded-string (url-encode search-string)]
+    (str base-url "?q=" encoded-string rest-url api-key)))
 
 (defn groom
   [results]
