e1f9b4e3a090fd5165b7904592370a20bc8b4fe5
Henrik Eneroth <henrik@eneroth.com>
2014-01-22 13:14:10 +0100

    Now constructing query more dynamically.

--- src/server/google_books.clj
+++ src/server/google_books.clj
@@ -6,18 +6,45 @@
             [server.helper :refer [kebabize-keys]]
             [clojure.walk :refer [keywordize-keys]]))
 
-(def api-key "AIzaSyDzJOr0Xk-fM8gCoLu7DwxTwvIv6V5dG-o")
+;; Google Books constants
 
-(def test-url "https://www.googleapis.com/books/v1/volumes?q=neuromancer&download=epub&
-              filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&
-              printType=books&key=")
+(def api-key "AIzaSyDzJOr0Xk-fM8gCoLu7DwxTwvIv6V5dG-o") ;; Read this from environment instead
+
+(def base-url "https://www.googleapis.com/books/v1/volumes")
+
+(def query-params 
+  {:query              "q"               ; Full-text search query string. (string)
+   :download-available "download"        ; Restrict to volumes by download availability. (string)
+   :filter             "filter"          ; Filter search results. (string)
+   :language-restrict  "langRestrict"    ; Restrict results to books with this language code. (string)
+   :library-restrict   "libraryRestrict" ; Restrict search to this user's library. (string)
+   :max-results        "maxResults"      ; Maximum number of results to return. (integer, 0-40)
+   :order-by           "orderBy"         ; Sort search results. (string)
+   :partner            "partner"         ; Restrict and brand results for partner ID. (string)
+   :print-type         "printType"       ; Restrict to books or magazines. (string)
+   :projection         "projection"      ; Restrict information returned to a set of selected fields. (string)
+   :show-preorders     "showPreorders"   ; Set to true to show books available for preorder. Defaults to false. (boolean)
+   :source             "source"          ; String to identify the originator of this request. (string)
+   :start-index        "startIndex"      ; Index of the first result to return (starts at 0) (integer, 0+)
+   :fields             "fields"          ; Selector specifying which fields to include in a partial response.
+   :key                "key"})           ; API-key
+
+
+;; Functions
+(defn construct-url
+  [query-map]
+  (let [query-strings (map #(str ((key %) query-params) "=" (val %)) query-map)
+        query-vector  (interpose "&" query-strings )
+        query-string  (apply str query-vector)]
+    (str base-url "?" query-string)))
 
 (defn search-url
   [search-string]
-  (let [base-url "https://www.googleapis.com/books/v1/volumes"
-        rest-url "&download=epub&filter=ebooks&libraryRestrict=no-restrict&maxResults=10&orderBy=relevance&printType=books&key="
-        encoded-string (url-encode search-string)]
-    (str base-url "?q=" encoded-string rest-url api-key)))
+  (let [query {:query (url-encode search-string)
+               :max-results 10
+               :print-type "books"
+               :key api-key}]
+    (construct-url query)))
 
 (defn groom
   [results]
